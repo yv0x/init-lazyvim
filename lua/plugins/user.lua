6c6583f61f39868970f7dea9dfa1d94a1ec118c4
yv0x <7ce675@proton.me>
2023-12-27 20:11:09 +0000

    vim-fugitive

---
+++ lua/plugins/user.lua
@@ -0,0 +1,33 @@
+return {
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "mbbill/undotree",
+  },
+  {
+    "saecki/crates.nvim",
+    tag = "stable",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup()
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+  },
+  -- { "codota/tabnine-nvim", build = "./dl_binaries.sh" },
+  -- setup = {
+  --   disable_auto_comment = true,
+  --   accept_keymap = "<Tab>",
+  --   dismiss_keymap = "<C-]>",
+  --   debounce_ms = 800,
+  --   suggestion_color = { gui = "#808080", cterm = 244 },
+  --   exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+  --   log_file_path = nil, -- absolute path to Tabnine log file
+  -- },
+}
